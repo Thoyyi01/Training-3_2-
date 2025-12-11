70252dc3cfa19e59755e835dd6ad4f608937c213
Thoyyi01 <shafi.tayyab@gmail.com>
2025-12-11 10:58:58 +0530

    Update Str.py

--- Str.py
+++ Str.py
@@ -1,5 +1,25 @@
 print("Hello, World!")
 
-a = "Hello"
-print(a)
+a = "Hello World! Tokenisation Example."
+tokens = a.split()
+print(tokens)
 
+#text = "apple,mango-orange:banana?"
+
+# Replace delimiters with spaces
+#delimiters = ",:-?"
+#or d in delimiters:
+#    text = text.replace(d, " ")
+
+#tokens = text.split()
+#print(tokens)
+
+#chars = [ch for ch in text]
+#print(chars)
+
+import re
+
+text = "Hello, how are you?"
+
+tokens = re.findall(r"\w+|\S", text)
+print(tokens)
