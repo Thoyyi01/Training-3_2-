f5a9a0c7569920e95e30abd734c77bb2662e4fef
Thoyyi01 <shafi.tayyab@gmail.com>
2025-12-17 22:58:23 +0530

    Update MMM.py

--- MMM.py
+++ MMM.py
@@ -1,6 +1,15 @@
 import numpy as np
 
-Mean = [1,3,5,6,7,8,9,10]
+Mean = [2,5,3,6,9,8,1,7,10]
 print("Mean of the list is:", sum(Mean)/len(Mean))
 print("Median", np.median(Mean))
 print(np.mean(Mean))
+
+n = len(Mean)
+for i in range(n):
+    for j in range(0, n - i - 1):
+        if Mean[j] > Mean[j + 1]:
+            # swap
+            Mean[j], Mean[j + 1] = Mean[j + 1], Mean[j]
+
+print("Sorted list using Bubble Sort:", Mean)
